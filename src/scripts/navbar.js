7da019bd6f490e7f7ef1d382ec1ccb86f475f726
P4ll0 <159697381+P4ll0@users.noreply.github.com>
2024-02-12 13:48:59 +0100

    Update navbar.js

--- src/scripts/navbar.js
+++ src/scripts/navbar.js
@@ -2,7 +2,9 @@
 let isChecked = false;
 const navbar = document.querySelector('.header');
 const checkMenu = document.querySelector(".header .menuBTN");
+const scritta = document.querySelector(".logo");
 let prevColor = navbar.getAttribute("backgroundColor");
+let displayScritta = scritta.getAttribute("display");
 
 // FA SPARIRE IL MENU QUANDO PREMO SUL MENU AD HAMBURGER
 checkMenu.addEventListener("click", () => {
@@ -10,8 +12,10 @@ checkMenu.addEventListener("click", () => {
 
     if (isChecked) {
         navbar.style.backgroundColor = "transparent";
+        scritta.style.display = "none";
     }
     else {
         navbar.style.backgroundColor = prevColor;
+        scritta.style.display = displayScritta;
     }
 });
